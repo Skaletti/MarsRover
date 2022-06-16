cf70e973181dd58c94c6efab8441476850685e9a
Skaletti <gordey.korolev.2016@mail.ru>
2022-06-16 22:07:22 +0700

    Nasa api readygit add .!

--- script.js
+++ script.js
@@ -5,55 +5,52 @@ const roverPickerOpportunity = document.getElementById('rover-opportunity')
 const roverPickerSpirit = document.getElementById('rover-spirit')
 const selectRover = document.getElementById('main-list__rovers')
 
- let roverSelected = selectRover.addEventListener("click", () => {
-    let target = Event.target
-    if (target == roverPickerСuriosity.target)
-    {
-        roverSelected = 0
-        console.log("000")
-    }      
-    else if (target == roverPickerOpportunity.target)
-    {
-        roverSelected = 1
-        console.log("111")
-    }
-    else if (target == roverPickerSpirit.target)
-    {
-        roverSelected = 2
-        console.log("222")
-    }
-    
-})
+const mainImages = document.querySelectorAll('.main-list__item')
 
-console.log(roverSelected)
-
+const API_BASE_URL = 'https://api.nasa.gov/mars-photos/api/v1'
 
 
 const datePicker = document.getElementById("date")
-datePicker.min = "2012-08-06";
+datePicker.min = "2008-08-06";
+datePicker.value = datePicker.min
 const button = document.getElementById("button")
 
 const APIKEY = "Kxxlkd2hUzVR8tw66hpQWLNdXHG8WAVaE7iZaemg"
 
-button.addEventListener("click", function () {
+mainImages.forEach(rover => {
+    rover.addEventListener('click', async () => {
+        await getData(rover.dataset.name)
+    })
+})
+
+const roverData = {
+    image: '',
+    sol: '',
+    cameraName: '',
+    roverName: ''
+}
+
+
+const getData = (name) => {
+    if (typeof name !== 'string') return
+
     const date = datePicker.value;
-    console.log(date)
-    let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${APIKEY}`
-    console.log(url)
+
+    const url = `${API_BASE_URL}/rovers/${name}/photos?earth_date=${date}&api_key=${APIKEY}`
 
     axios.get(url)
         .then(function (response) {
-            var image = response.data.photos[0].img_src;
-            console.log(image)
-            var sol = response.data.photos[0].sol;
-            console.log(sol)
-            var cameraName = response.data.photos[0].camera.full_name;
-            console.log(cameraName)
-            var roverName = response.data.photos[0].rover.name;
-            console.log(roverName);
-            var text = `The image below was taken by the ${cameraName} on the ${roverName} rover on Martian sol ${sol}.`;
+            roverData.image = response.data.photos[0].img_src;
+            console.log(roverData.image)
+            roverData.sol = response.data.photos[0].sol;
+            console.log(roverData.sol)
+            roverData.cameraName = response.data.photos[0].camera.full_name;
+            console.log(roverData.cameraName)
+            roverData.roverName = response.data.photos[0].rover.name;
+            console.log(roverData.roverName);
+            const text = `The image below was taken by the ${roverData.cameraName} on the ${roverData.roverName} rover on Martian sol ${roverData.sol}.`;
             document.getElementById("results").innerHTML = text;
-            document.getElementById("image").src = image;
+            document.getElementById("image").src = roverData.image;
             document.getElementById("image").style.display = "block";
         })
         .catch(function (error) {
@@ -61,8 +58,10 @@ button.addEventListener("click", function () {
             var error = "Sorry, there are no photos on this date! Pick another.";
             document.getElementById("results").innerHTML = error;
         })
+}
 
-});
+
+button.addEventListener("click", getData)
 
 
 
